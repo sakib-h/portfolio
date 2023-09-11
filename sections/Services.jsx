febde320feb8740db896e3b461743bbf862b06e2
Sakib Hasan <sakib100.sa@gmail.com>
2023-09-12 00:47:05 +0600

    standered package added

--- sections/Services.jsx
+++ sections/Services.jsx
@@ -71,6 +71,61 @@ const Services = () => {
 							</li>
 						</ul>
 					</div>
+
+					<div className="w-full">
+						<h6 className="pt-10 text-xl mb-3">Standard Plan </h6>
+						<h1 className="text-secondary text-4xl mb-10">199$</h1>
+						<ul className="flex flex-col gap-3 list-outside">
+							<li className="flex items-start text-sm gap-2">
+								<Image
+									src="/images/extras/check.svg"
+									width={20}
+									height={20}
+									alt="check"
+								/>
+								Basic Website Development with Essential
+								Features and Functionality
+							</li>
+							<li className="flex items-start text-sm gap-2">
+								<Image
+									src="/images/extras/check.svg"
+									width={20}
+									height={20}
+									alt="check"
+								/>
+								Fully Responsive, optimized for various screen
+								sizes and devices
+							</li>
+							<li className="flex items-start text-sm gap-2">
+								<Image
+									src="/images/extras/check.svg"
+									width={20}
+									height={20}
+									alt="check"
+								/>
+								Basic SEO Optimization to improve search engine
+								visibility
+							</li>
+							<li className="flex items-start text-sm gap-2">
+								<Image
+									src="/images/extras/check.svg"
+									width={20}
+									height={20}
+									alt="check"
+								/>
+								Domain and Hosting Setup
+							</li>
+							<li className="flex items-start text-sm gap-2">
+								<Image
+									src="/images/extras/check.svg"
+									width={20}
+									height={20}
+									alt="check"
+								/>
+								Contact Form and Basic Integration
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</section>
